557c92d3c0d0826ecca68ff2050ae8c35689f133
Arpit Santoki <santokiarpit0@gmail.com>
2023-06-07 00:14:46 +0530

    added establishment interface

---
+++ src/app/modules/home/home.ts
@@ -0,0 +1,39 @@
+export interface EstablishmentDTO {
+    address: string;
+    description: string;
+    emails: string [];
+    facebookPage: string;
+    id: string;
+    lat: number;
+    lon: number;
+    name: string;
+    phones: string[];
+    photos: string[];
+    supportedCurrencies: supportedCurrenciesDTO[];
+    supportedLanguages: supportedLanguagesDTO[];
+    twitterAccount: string;
+    type: string;
+    videos: string[];
+    website: string;
+    workingHours: workingHoursDTO[];
+}
+
+
+export interface supportedCurrenciesDTO {
+    code: string;
+    name: string;
+}
+
+export interface supportedLanguagesDTO {
+    code: string;
+    name: string;
+}
+export interface workingHoursDTO {
+    day: number;
+    workingIntervals: workingIntervalsDTO[];
+}
+
+export interface workingIntervalsDTO {
+    from: string
+    to: string
+}
